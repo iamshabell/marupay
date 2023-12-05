63b7d011322dbda7dc9a823b3d045e75c6e16952
iamshabell <91321698+iamshabell@users.noreply.github.com>
2023-12-06 00:11:46 +0300

    refactor(edahab): handler code for improved readability and maintainability

--- packages/fintekpay/src/handlers/edahab/edahab.ts
+++ packages/fintekpay/src/handlers/edahab/edahab.ts
@@ -26,31 +26,25 @@ export const createEdahabHandler = defineHandler({
     request: async ({ ctx, options }: { ctx: Ctx, options: Options }) => {
         const { amount, accountNumber, currency, description } = options;
         const { links, apiKey, merchantId } = ctx;
-        
-        const hashCode = hashSecretKey({ 
-            apiKey: ctx.apiKey, 
-            edahabNumber: accountNumber, 
-            amount, 
-            currency, 
-            agentCode: ctx.merchantId, 
-            description}, ctx.secretKey);
 
-        const response = await axios.post<API.RequestPaymentReq, { data: API.RequestPaymentRes }>(links.request + hashCode, {
-            apiKey: apiKey,
+        const hashCode = hashSecretKey({ apiKey, edahabNumber: accountNumber, amount, currency, agentCode: merchantId, description }, ctx.secretKey);
+
+        const response = await axios.post<API.RequestPaymentReq, { data: API.RequestPaymentRes }>(`${links.request}${hashCode}`, {
+            apiKey,
             edahabNumber: accountNumber,
-            amount: amount,
-            currency: currency,
+            amount,
+            currency,
             agentCode: merchantId,
-            description: description,
+            description,
         });
 
         const { TransactionId, InvoiceStatus, StatusCode, StatusDescription } = response.data;
 
-        const responseCode = StatusCode.toString();
+        const responseCode = `${StatusCode}`;
 
         if (responseCode !== '0') {
-            console.log(StatusDescription);
-            throw Error(responseCode);
+            console.log(`${StatusDescription}`);
+            throw responseCode;
         }
 
         return {
@@ -59,7 +53,6 @@ export const createEdahabHandler = defineHandler({
             referenceId: generateUuid(),
         };
     },
-
 });
 
-export type EdahabHandler = ReturnType<typeof createEdahabHandler>;
+export type EdahabHandler = ReturnType<typeof createEdahabHandler>
