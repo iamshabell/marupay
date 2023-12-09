642405b442cf89216d3c8e4d0585432976dcc064
iamshabell <91321698+iamshabell@users.noreply.github.com>
2023-12-10 01:19:18 +0300

    add: doc web app

---
+++ apps/docs/theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>MaruPay</span>,
+  project: {
+    link: 'https://github.com/marupay',
+  },
+  docsRepositoryBase: 'https://github.com/marupay',
+  footer: {
+    text: 'MaruPay Docs',
+  },
+}
+
+export default config
